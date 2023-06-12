54b99702b1afd89c813bf0514f059d29b6d9ed75
Caturiani Pratidina Bintari <79180390+crumybrulee@users.noreply.github.com>
2023-06-12 12:46:17 +0700

    sankey ok

--- Tugas1/explore/pages/details/sankey.py
+++ Tugas1/explore/pages/details/sankey.py
@@ -7,13 +7,13 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 
 # Read the dataset
-df = pd.read_csv("../PHL-Sankey.csv")
-# df = pd.read_csv("Tugas1\PHL-Sankey.csv")
+# df = pd.read_csv("../PHL-Sankey.csv")
+df = pd.read_csv("../PHL-Sankey2.csv")
 
 # Membuat dataframe pertama
 df1 = df.groupby(['Bahan Baku', 'Kelompok Kayu Bulat'])['Volume(m3)'].count().reset_index()
 df1.columns = ['source', 'target', 'value']
-df1['source'] = df1['source'].map({'Kayu Tanaman': 'Kayu Tanaman', 'Kayu Alam': 'Kayu Alam', 'Kayu Perkebunan': 'Kayu Tanaman', 'Setengah Jadi': 'Setengah Jadi', 'Limbah': 'Kayu Tanaman'})
+df1['source'] = df1['source'].map({'Kayu Tanaman': 'Kayu Tanaman', 'Kayu Alam': 'Kayu Alam', 'Setengah Jadi': 'Setengah Jadi', 'Limbah': 'Limbah'})
 
 # Membuat dataframe kedua
 df2 = df.groupby(['Kelompok Kayu Bulat', 'Jenis Kayu Olahan'])['Volume(m3)'].count().reset_index()
@@ -21,9 +21,9 @@ df2.columns = ['source', 'target', 'value']
 df2['target'] = df2['target'].map({ 'Chipwood' : 'Chipwood', 'Kayu Lapis dan LVL' : 'Chipwood', 'Kayu Gergajian':'Chipwood', 'Panel':'Panel', 'Pulp':'Pulp', 'Blockboard':'Blockboard', 'Veneer': 'Veneer', 'Bare Core':'Bare Core' ,'Wood Pellet':'Wood Pellet' ,'Particle Board':'Particle Board','Kayu Olahan Lainnya':'Kayu Olahan Lainnya','Moulding':'Moulding','Finger Joint Board':'Finger Joint Board' })
 
 # Membuat dataframe ketiga
-# df3 = df.groupby(['Jenis Kayu Olahan', 'Produk Ekspor'])['Volume(m3)'].count().reset_index()
-# df3.columns = ['source', 'target', 'value']
-# df3['target'] = df3['target'].map({'Bangunan Prefarikasi': 'Bangunan Prefarikasi', 'Chipwood' : 'Chipwood', 'Furnitur Kayu' : 'Furnitur Kayu', 'Kerajinan':'Kerajinan', 'Panel':'Panel', 'Pulp':'Pulp', 'Paper':'Paper', 'Veneer': 'Veneer', 'Woodworking':'WoodWorking'})
+df3 = df.groupby(['Jenis Kayu Olahan', 'Produk Ekspor'])['Volume(m3)'].count().reset_index()
+df3.columns = ['source', 'target', 'value']
+df3['target'] = df3['target'].map({'Bangunan Prefarikasi': 'Bangunan Prefarikasi', 'Chipwood' : 'Chipwood', 'Furnitur Kayu' : 'Furnitur Kayu', 'Kerajinan':'Kerajinan', 'Panel':'Panel', 'Pulp':'Pulp', 'Paper':'Paper', 'Veneer': 'Veneer', 'Woodworking':'WoodWorking'})
 
 # 2 dataframe untuk supply demand kayu olah
 # Menggabungkan kedua dataframe
